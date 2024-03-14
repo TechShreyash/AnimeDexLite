c616506dc7cd7c4d4f8f1a4d6ca26d1cc60687aa
TechShreyash <82265247+TechShreyash@users.noreply.github.com>
2024-03-14 20:27:14 +0530

    Logo Changed, Video Player Modified, Fix Issue Of Not Loading Episode Preview On Video Player

--- js/episode.js
+++ js/episode.js
@@ -57,19 +57,17 @@ function capitalizeFirstLetter(string) {
 
 // Function to get m3u8 url of episode
 async function loadVideo(name, stream) {
-    const episodeid =
-        urlParams.get("anime") + "-episode-" + urlParams.get("episode");
 
     try {
         document.getElementById("ep-name").innerHTML = name;
         const serversbtn = document.getElementById("serversbtn");
 
         let url = stream["sources"][0]["file"];
-        serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn sactive" onclick="selectServer(this)" data-value="./embed.html?url=${url}&id=${episodeid}">AD Free 1</a> </div>`;
+        serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn sactive" onclick="selectServer(this)" data-value="./embed.html?url=${url}&episode_id=${EpisodeID}">AD Free 1</a> </div>`;
         document.getElementsByClassName("sactive")[0].click();
 
         url = stream["sources_bk"][0]["file"];
-        serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn" onclick="selectServer(this)" data-value="./embed.html?url=${url}&id=${episodeid}">AD Free 2</a> </div>`;
+        serversbtn.innerHTML += `<div class="sitem"> <a class="sobtn" onclick="selectServer(this)" data-value="./embed.html?url=${url}&episode_id=${EpisodeID}">AD Free 2</a> </div>`;
 
         return true;
     } catch (err) {
